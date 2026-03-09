66ca1564c512218ffdc3d5144132b8682472aa66
Александр Лобин <lobin.a.g@effective-group,ru>
2026-03-10 02:28:55 +0300

    recovery dashboards

--- migrations/005_remove_delivery_logs.sql
+++ migrations/005_remove_delivery_logs.sql
@@ -1,7 +1,2 @@
 -- Удаляем таблицу delivery_logs
 DROP TABLE IF EXISTS delivery_logs;
-
--- Можно также удалить связанные индексы, если они были созданы отдельно
--- (но при удалении таблицы они удалятся автоматически)
-
-COMMENT ON TABLE delivery_logs IS NULL;
